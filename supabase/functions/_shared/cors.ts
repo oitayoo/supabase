b590f21a899767203d0c5dfb56809b0a2618051f
oitayoo <dongwook@oitayoo.com>
2024-07-25 16:42:12 +0900

    remove github action

--- supabase/functions/_shared/cors.ts
+++
@@ -1,6 +0,0 @@
-export const headers: { [key: string]: string } = {
-	'Access-Control-Allow-Origin': '*',
-	'Access-Control-Allow-Headers': 'authorization, x-client-info, apikey, content-type',
-}
-
-export default { headers }
